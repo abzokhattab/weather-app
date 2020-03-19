1dbe3f810435daff84e3d511609d67264751fcc4
Abzokhattab <abzokhattab@gmail.com>
2020-03-19 23:06:28 +0100

    routing by selectors

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,10 +1,26 @@
 console.log('Client side javascript file is loaded!')
-fetch('http://localhost:3000/weather/?address=boston').then((Response)=>{
-    Response.json().then((data)=>{
-        console.log(data.location)
-    })
-}) 
+
 const loc =document.querySelector('form')
-loc.addEventListener('submit',()=>{
-    console.log("testing")
-})
+const search= document.querySelector('input')
+const msg1= document.querySelector('#msg 1')
+const msg2= document.querySelector('#msg2')
+
+msg1.textContent='Loading...'
+
+loc.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const location = search.value
+
+
+    fetch('http://localhost:3000/weather/?address='+location).then((Response)=>{
+        Response.json().then((data)=>{
+            if (data.error){
+                msg1.textContent=data.error
+                
+            }else {
+               msg1.textContent=data.location
+               msg2.textContent=data.forecast
+
+            }
+        })
+    }) }) 
